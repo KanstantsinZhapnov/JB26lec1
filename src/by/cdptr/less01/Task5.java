251669fe83366b506bffb013dd7891a78397a7ac
Kanstantsin Zhapnov <kanstantsin_zhapnov@epam.com>
2018-09-17 19:14:40 +0300

    FIO implementation

--- src/by/cdptr/less01/Task5.java
+++ src/by/cdptr/less01/Task5.java
@@ -3,7 +3,7 @@ package by.cdptr.less01;
 public class Task5 {
     public static void main(String[] args){
         double a, b, c, d ; // слагаемые
-        double summ; //сумма
+        double summ; //сумма слагаемых
         a=1; b=2; c=3; d=4;
 
         summ = a+b+c+d;
